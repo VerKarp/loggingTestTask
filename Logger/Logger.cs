621f13bb892d6e441aa30baae5daa43bce9d0e0b
VerKarp <vrkrpv@gmail.com>
2022-06-24 00:25:40 +0300

    The main functionality of logging to a text file

--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -1,6 +1,53 @@
-﻿namespace Logger
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace SimpleLogger
 {
     public class Logger
     {
+        public enum Level
+        {
+            Info,
+            Warn,
+            Debug,
+            Error
+        }
+
+        public static void Log(Level level, string message)
+        {
+            var methodBase = GetCallingMethodBase();
+            var callingMethod = methodBase.Name;
+            var callingClass = methodBase.ReflectedType.Name;
+
+            Log(level, message, callingClass, callingMethod);
+        }
+
+        private static void Log(Level level, string message, string callingClass, string callingMethod)
+        {
+            string path = "log.txt";
+            DateTime date = DateTime.Now;
+
+            LogMessage logMessage = new(date, level, callingClass, callingMethod, message);
+
+            using (StreamWriter sw = new(path, true))
+            {
+                sw.WriteLine(logMessage.ToString());
+            }
+        }
+
+        private static MethodBase GetCallingMethodBase()
+        {
+            var stackTrace = new StackTrace();
+            for (var i = 0; i < stackTrace.GetFrames().Length; i++)
+            {
+                var methodBase = stackTrace.GetFrame(i).GetMethod();
+                var name = MethodBase.GetCurrentMethod().Name;
+                if (!methodBase.Name.Equals("Log") && !methodBase.Name.Equals(name))
+                    return methodBase;
+            }
+            return MethodBase.GetCurrentMethod();
+        }
     }
 }
