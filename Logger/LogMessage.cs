616f8d54da199a27cef2048b61b4769361c8186c
VerKarp <vrkrpv@gmail.com>
2022-06-24 00:24:26 +0300

    Adding a class to create a log message

---
+++ Logger/LogMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SimpleLogger
+{
+    class LogMessage
+    {
+        private readonly DateTime _date;
+        private readonly Logger.Level _level;
+        private readonly string _class;
+        private readonly string _method;
+        private readonly string _text;
+
+        public LogMessage() { }
+
+        public LogMessage(DateTime date, Logger.Level level, string callingClass, string method, string text)
+        {
+            _date = date;
+            _level = level;
+            _class = callingClass;
+            _method = method;
+            _text = text;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0:dd.MM.yyyy HH:mm}: [{1}] - {2}.{3} - {4}", _level.ToString(), _date, _class, _method, _text);
+        }
+    }
+}
