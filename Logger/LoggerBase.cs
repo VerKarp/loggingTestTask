fdc3044fe082d833df87334a67256fa543ffd9ef
VerKarp <vrkrpv@gmail.com>
2022-06-26 17:34:57 +0300

    Adding an abstract logger base class

---
+++ Logger/LoggerBase.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleLogger
+{
+    public enum LogTarget
+    {
+        File,
+        Db
+    }
+
+    public enum LogLevel
+    {
+        Info,
+        Warn,
+        Debug,
+        Error
+    }
+
+    public abstract class LoggerBase
+    {
+        public abstract void Log(LogMessage message);
+    }
+}
