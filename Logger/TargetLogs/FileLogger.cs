203956e0058c88d4599601ab2b9c36a4de94fd65
VerKarp <vrkrpv@gmail.com>
2022-06-26 17:35:38 +0300

    Adding a file logger class and logging logic to a file

---
+++ Logger/TargetLogs/FileLogger.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace SimpleLogger.TargetLogs
+{
+    public class FileLogger : LoggerBase
+    {
+        private readonly string path = "log.txt";
+
+        public override void Log(LogMessage logMessage)
+        {
+            using (StreamWriter sw = new(path, true))
+                sw.WriteLine(logMessage.ToString());
+        }
+    }
+}
