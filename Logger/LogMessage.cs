9d8e1400cadac6988b6a69cf0595b4e66b967c85
VerKarp <vrkrpv@gmail.com>
2022-06-26 17:14:18 +0300

    Editing the log message class (changing fields to properties)

--- Logger/LogMessage.cs
+++ Logger/LogMessage.cs
@@ -4,26 +4,26 @@ namespace SimpleLogger
 {
     class LogMessage
     {
-        private readonly DateTime _date;
-        private readonly Logger.Level _level;
-        private readonly string _class;
-        private readonly string _method;
-        private readonly string _text;
+        public DateTime Time { get; set; }
+        public LogLevel Level { get; set; }
+        public string Class { get; set; }
+        public string Method { get; set; }
+        public string Message { get; set; }
 
         public LogMessage() { }
 
-        public LogMessage(DateTime date, Logger.Level level, string callingClass, string method, string text)
+        public LogMessage(DateTime date, LogLevel level, string callingClass, string method, string text)
         {
-            _date = date;
-            _level = level;
-            _class = callingClass;
-            _method = method;
-            _text = text;
+            Time = date;
+            Level = level;
+            Class = callingClass;
+            Method = method;
+            Message = text;
         }
 
         public override string ToString()
         {
-            return string.Format("{0:dd.MM.yyyy HH:mm}: [{1}] - {2}.{3} - {4}", _level.ToString(), _date, _class, _method, _text);
+            return string.Format("{0:dd.MM.yyyy HH:mm}: [{1}] - {2}.{3} - {4}", Level.ToString(), Time, Class, Method, Message);
         }
     }
 }
